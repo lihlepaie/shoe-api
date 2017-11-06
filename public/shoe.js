6b178a012d5399058ed0b83a1ed7d3237d9d4e49
lihlepaie <pasielihle@gmail.com>
2017-11-06 16:12:01 +0200

    Need to get  each size,color and brand etc

--- public/shoe.js
+++ public/shoe.js
@@ -1,33 +1,35 @@
-$(function(){
+$(function() {
 
-var sizedrpDwn = document.querySelector('.myTemplate1')
-var colordrpDwn = document.querySelector('.myTemplate2')
-var brandDrpDwn = document.querySelector('.myTemplate3')
-var colors = document.querySelector('.color');
-var brand = document.querySelector('.brand')
-var sizeSelected = document.querySelector('.sizes');
-var searchShoes = document.querySelector('.shoeStock')
-var li = document.querySelector('.li')
-var shoe = document.querySelector('.shoeData')
-var myshoeSize = document.querySelector('.myshoeSize')
-var result = document.querySelector('.results')
-var addbttn = document.querySelector('.addBtn')
-var bttn = document.querySelector('.bttn')
-var image = document.querySelector('.bttn')
-var display = document.querySelector('.display')
+    var sizedrpDwn = document.querySelector('.myTemplate1')
+    var colordrpDwn = document.querySelector('.myTemplate2')
+    var brandDrpDwn = document.querySelector('.myTemplate3')
+    var colors = document.querySelector('.color');
+    var brand = document.querySelector('.brand')
+    var sizeSelected = document.querySelector('.sizes');
+    var searchShoes = document.querySelector('.shoeStock')
+    var li = document.querySelector('.li')
+    var shoe = document.querySelector('.shoeData')
+    var myshoeSize = document.querySelector('.myshoeSize')
+    var result = document.querySelector('.results')
+    var addbttn = document.querySelector('.addBtn')
+    var bttn = document.querySelector('.bttn')
+    var image = document.querySelector('.bttn')
+    var display = document.querySelector('.display')
 
 
 
-var myTemplate = document.querySelector(".myTemplate");
-var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
+    var myTemplate = document.querySelector(".myTemplate");
+    var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
 
-  $.ajax({
-    type:'GET',
-    url:'/api/shoes',
-    datatype:'json',
-    success:function(shoeData){
-  display.innerHTML =TemplateInstance({shoes:shoeData})
-    }
+    $.ajax({
+        type: 'GET',
+        url: '/api/shoes',
+        datatype: 'json',
+        success: function(shoeData) {
+            display.innerHTML = TemplateInstance({
+                shoes: shoeData
+            })
+        }
 
     });
 
@@ -46,47 +48,27 @@ var TemplateInstance = Handlebars.compile(myTemplate.innerHTML);
         var price = add_price.value;
         var brand = add_brand.value
         console.log(brand);
-// if (add_color !== undefined && add_size !== undefined && add_brand !== undefined && add_price !== undefined && in_stock !== undefined) {
+        // if (add_color !== undefined && add_size !== undefined && add_brand !== undefined && add_price !== undefined && in_stock !== undefined) {
 
-          var newdata = {
-                color: color,
-                size: size,
-                brand: brand,
-                price: price,
-                in_stock: stock
-                // image: imagePath,
-              }
-    $.ajax({
-      type:'POST',
-      url:'/api/shoes',
-      datatype:'json',
-      data : newdata,
-      success:function(shoeData){
-    display.innerHTML =TemplateInstance({shoes:shoeData})
-      }
+        var newdata = {
+            color: color,
+            size: size,
+            brand: brand,
+            price: price,
+            in_stock: stock
+            // image: imagePath,
+        }
+        $.ajax({
+            type: 'POST',
+            url: '/api/shoes',
+            datatype: 'json',
+            data: newdata,
+            success: function(shoeData) {
+                display.innerHTML = TemplateInstance({
+                    shoes: shoeData
+                })
+            }
+        })
+
     })
-
-
-    var myDropdown = Handlebars.compile(sizedrpDwn.innerHTML);
-    var results = myDropdown({
-      shoes: shoes
-    });
-    var sizeSelected = document.querySelector('.selectSize');
-    sizeSelected.innerHTML = results;
-
-    var myDropdown = Handlebars.compile(colordrpDwn.innerHTML);
-    var results = myDropdown({
-      shoes: shoes
-    });
-    var colors = document.querySelector('.selectColor');
-    colors.innerHTML = results;
-
-    var myDropdown = Handlebars.compile(brandDrpDwn.innerHTML);
-    var results = myDropdown({
-      shoes: shoes
-    });
-    var brand = document.querySelector('.selectBrand')
-    brand.innerHTML = results;
-
-})
 })
